edc43fa99148475d80c105932bd081726a98eff8
Tecnico - TI <tecnico@epfserver.com.br>
2016-05-02 22:44:58 -0300

    Inclusao de soma

---
+++ src/Principal.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class Principal {
+	
+	public static void main (String [] args){
+		//Instanciando um objeto
+		
+		calculadora Calculadora = new /*cria um objeto baseado na classe calculadora*/ calculadora ();
+		
+		Calculadora.somar(10,50);
+		Calculadora.somar(20,10);
+		
+		
+		JOptionPane.showMessageDialog(null, Calculadora.getValor());
+				
+		
+	}
+
+}
--- src/calculadora.java
+++ src/calculadora.java
@@ -1,26 +1,28 @@
 
 public class calculadora {
 	
-	private int valorum;
-	private int valordois;
+	private int valor;
 	
-	public void somar(){
+	
+	public void somar(int valorum, int valordois){
 		
-		
+		this.valor = this.valor + (valorum + valordois);
 	}
+		
+		/*this.valorum = valorum;
+		this.valordois = valordois;
+		this.total +=//this.total+// this.valorum + this.valordois; //O THIS É DESTA CLASSE. (O VALOR 1 DESTA CLASSE) DO ESCOPO DESTA CLASSE.
+		
+	}*/
 	
 	public void subtrair(){
-		
 	}
-	
 	public void dividir(){
-		
 	}
-	
 	public void multiplicar(){
-		
-		
 	}
 	
-
+	public int getValor(){
+	 return valor;
+	}
 }
